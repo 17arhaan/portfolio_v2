f459e63390e582cd269aa7b8e2fce97cdf578a5b
17Arhaan <17arhaan@gmail.com>
2025-03-25 19:43:23 +0530

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,7 @@ import CodeStats from "@/components/code-stats"
 import Contact from "@/components/contact"
 import Summary from "@/components/summary"
 import { Toaster } from "@/components/ui/toaster"
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
